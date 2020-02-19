dd43b93fdac13c08dc872ca2ee4d9ea40542a74d
uuboy <uu_boy@foxmail.com>
2020-02-19 15:29:43 +0000

    init

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,50 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
+  <div class="container">
+    <!-- Branding Image -->
+    <a class="navbar-brand " href="{{ url('/') }}">
+      Laravel-WM-Server
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Of Navbar -->
+      <ul class="navbar-nav mr-auto">
+
+      </ul>
+
+
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav navbar-right">
+        <!-- Authentication Links -->
+        @guest
+          <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+          <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        @else
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img src="{{ Auth::user()->avatar }}" class="img-responsive img-circle" width="30px" height="30px">
+              {{ Auth::user()->name }}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              @can('manage_contents')
+                <a class="dropdown-item" href="{{ url(config('administrator.uri')) }}">
+                  <i class="fas fa-tachometer-alt mr-2"></i>
+                  管理后台
+                </a>
+                <div class="dropdown-divider"></div>
+              @endcan
+              <a class="dropdown-item" id="logout" href="#">
+                <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
+                  {{ csrf_field() }}
+                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                </form>
+              </a>
+            </div>
+          </li>
+        @endguest
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('title', '首页')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8 col-lg-8">
+      <div class="card">
+        <div class="card-header">
+          标题
+        </div>
+        <div class="card-body">内容</div>
+      </div>
+    </div>
+  </div>
+</div>
+
+@stop
